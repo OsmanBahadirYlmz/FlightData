12020ede1fe3ee68f3fcfab7cdfab7f62fa61cf7
Osman Bahadir Yilmaz <71720842+OsmanBahadirYlmz@users.noreply.github.com>
2023-07-03 18:03:18 +0200

    data read and cleaning

---
+++ v1.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import r2_score
+
+data=pd.read_csv('flightdata.csv')
+df = pd.read_csv('flightdata.csv')
+
+df.isna().sum()
+
+
+filtered_df = df[(df['ARR_DEL15'].isnull()) & ((df['CANCELLED'] != 0) & (df['DIVERTED'] != 0))]
+
+# Get the count of rows satisfying the criteria
+row_count = filtered_df.shape[0]
+
+
+
+
+df['ADJUSTED']=df['ARR_DELAY']-df['DEP_DELAY']
+
+corr=df.corr()
+adjusted=df[df['ADJUSTED'] < 0]['ADJUSTED']
+
+
+
+
+print( df.columns)
+dummycolumns=['Unnamed: 25','YEAR','ARR_DEL15', 'ACTUAL_ELAPSED_TIME',
+              'DEP_TIME', 'DEP_DELAY',
+              'DEP_DEL15','ARR_TIME',
+              'TAIL_NUM', 'FL_NUM','ORIGIN_AIRPORT_ID',
+              'DEST_AIRPORT_ID','UNIQUE_CARRIER']
+
+for i in dummycolumns:
+    try:
+        df = df.drop(i, axis=1)
+    except:
+        print(i)
+        continue
+
+df2=df[(df['ARR_DELAY'].isnull()) ]
+df3=df[df['ARR_DELAY'].notnull()]
+
+
+print(df3.isna().sum())
+
+# catagorical to numeric
+
+dummies = pd.get_dummies(df3[['ORIGIN', 'DEST']])
+
+# Concatenate the dummies with the original DataFrame
+merged = pd.concat([df3, dummies], axis=1)
+
+# Drop the original 'ORIGIN' and 'DEST' columns
+merged = merged.drop(['ORIGIN', 'DEST'], axis=1)
+
+y=merged['ARR_DELAY']
+X=merged.drop(['CANCELLED', 'DIVERTED','ADJUSTED','ARR_DELAY'], axis=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
